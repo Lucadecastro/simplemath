fe899e43d7f7a993e21f115af1befc5eb555e693
Luca Castro <lucadecastro@gmail.com>
2024-09-12 23:41:28 -0300

    fix repository link

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="Um pacote simples de operações matemáticas",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/seu_usuario/simplemath",
+    url="https://github.com/Lucadecastro/simplemath",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
